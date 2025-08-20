757fc6bbc93b467ea9e2b395d5977c5a8449bc87
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2025-08-21 01:51:37 +0300

    Create ols_svd.py
    Signed-off-by: Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>

---
+++ ols_svd.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+A = np.array([[Числа], [Числа], [Числа]], dtype=float)
+b = np.array([Числа], dtype=float)
+
+# SVD
+U, S, VT
+
+# Строим псевдообратную Σ⁺
+np.diag(1/)
+
+# Moore–Penrose inverse: A^+ = V Σ^+ U^T
+матрица_V @ S_inv @ матрица_U
+
+# получаем веса
+A_pinv @ b
+
